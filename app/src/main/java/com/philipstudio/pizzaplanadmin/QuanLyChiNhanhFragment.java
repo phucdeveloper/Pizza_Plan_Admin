db0e196341049fbc2112d6f630678cdbf134f7af
Philip <44945136+phucdeveloper@users.noreply.github.com>
2020-08-10 15:24:49 +0700

    Update Project Pizza Plan for Admin by 15:24 PM Mon, August 10, 2020

--- app/src/main/java/com/philipstudio/pizzaplanadmin/QuanLyChiNhanhFragment.java
+++ app/src/main/java/com/philipstudio/pizzaplanadmin/QuanLyChiNhanhFragment.java
@@ -49,17 +49,33 @@ public class QuanLyChiNhanhFragment extends Fragment implements OnMapReadyCallba
         mGoogleMap.setOnMapClickListener(this);
         mGoogleMap.setTrafficEnabled(true);
 
+        setUpViTriCacCuaHang();
 
     }
 
     private void showMarker(LatLng latLng, String description){
-//        mGoogleMap.clear();
-//        MarkerOptions options = new MarkerOptions().position(latLng);
-//        options.title(description)
-//                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_baseline_place));
-//        mGoogleMap.addMarker(options);
+        mGoogleMap.clear();
+        MarkerOptions options = new MarkerOptions().position(latLng);
+        options.title(description)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_baseline_place));
+        mGoogleMap.addMarker(options);
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 18.0f);
         mGoogleMap.animateCamera(cameraUpdate);
-
     }
+
+    private void setUpViTriCacCuaHang() {
+        LatLng latLng1 = new LatLng(10.760398, 106.670878);
+        showMarker(latLng1, "91 Nguyễn Chí Thanh, phường 9, Quận 5, Hồ Chí Minh");
+        LatLng latLng2 = new LatLng(10.773527, 106.678397);
+        showMarker(latLng2, "162 Cao Thắng, Quận 10, Hồ Chí Minh");
+        LatLng latLng3 = new LatLng(10.770437, 106.676542);
+        showMarker(latLng3, "567 Điện Biên Phủ, phường 1, Quận 3, Hồ Chí Minh");
+        LatLng latLng4 = new LatLng(10.796334, 106.666035);
+        showMarker(latLng4, "266 Đường Lê Văn Sỹ, Phường 1, Tân Bình, Hồ Chí Minh");
+        LatLng latLng5 = new LatLng(10.810307, 106.695229);
+        showMarker(latLng5, "132 Nơ Trang Long, phường 14, Bình Thạnh, Hồ Chí Minh");
+        LatLng latLng6 = new LatLng(10.757893, 106.700130);
+        showMarker(latLng6, "56 Đường Khánh Hội, phường 4, Quận 4, Hồ Chí Minh");
+    }
+
 }
