4ce8adb6ae599a2a9385b2b8442dc379854306bf
Philip <44945136+phucdeveloper@users.noreply.github.com>
2020-08-17 21:39:59 +0700

    Update Project Pizza Plan for Admin by 21:39 PM Mon, August 17, 2020

--- app/src/main/java/com/philipstudio/pizzaplanadmin/adapter/ThongKeTheoMonAnAdapter.java
+++ app/src/main/java/com/philipstudio/pizzaplanadmin/adapter/ThongKeTheoMonAnAdapter.java
@@ -12,16 +12,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.philipstudio.pizzaplanadmin.R;
 import com.philipstudio.pizzaplanadmin.model.GioHang;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.ArrayList;
 
-public class MonAnAdapter extends RecyclerView.Adapter<MonAnAdapter.ViewHolder> {
+public class ThongKeTheoMonAnAdapter extends RecyclerView.Adapter<ThongKeTheoMonAnAdapter.ViewHolder>{
 
     ArrayList<GioHang> arrayList;
     Context context;
 
-    public MonAnAdapter(ArrayList<GioHang> arrayList, Context context) {
+    public ThongKeTheoMonAnAdapter(ArrayList<GioHang> arrayList, Context context) {
         this.arrayList = arrayList;
         this.context = context;
     }
@@ -29,18 +27,13 @@ public class MonAnAdapter extends RecyclerView.Adapter<MonAnAdapter.ViewHolder> 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_monan, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_thongke_monan, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.txtTenmonan.setText(arrayList.get(position).getMonAn().getTenMonAn());
-        NumberFormat formatter = new DecimalFormat("#,###");
-        double giatien = arrayList.get(position).getMonAn().getGia();
-        String formattedGiatien = formatter.format(giatien);
-        holder.txtGiatien.setText(formattedGiatien + " " + "đồng");
-        holder.txtSoluong.setText(String.valueOf(arrayList.get(position).getSoluong()));
+
     }
 
     @Override
@@ -50,13 +43,13 @@ public class MonAnAdapter extends RecyclerView.Adapter<MonAnAdapter.ViewHolder> 
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        TextView txtTenmonan, txtSoluong, txtGiatien;
+        TextView txtTenmonan, txtSoluong;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             txtTenmonan = itemView.findViewById(R.id.item_textview_tenmonan);
             txtSoluong = itemView.findViewById(R.id.item_textview_soluong);
-            txtGiatien = itemView.findViewById(R.id.item_textview_gia);
         }
     }
 }
--- app/src/main/java/com/philipstudio/pizzaplanadmin/view/ThongKeActivity.java
+++ app/src/main/java/com/philipstudio/pizzaplanadmin/view/ThongKeActivity.java
@@ -2,8 +2,10 @@ package com.philipstudio.pizzaplanadmin.view;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -12,7 +14,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.philipstudio.pizzaplanadmin.R;
+import com.philipstudio.pizzaplanadmin.model.ChiTietDonHang;
 import com.philipstudio.pizzaplanadmin.model.DonHang;
+import com.philipstudio.pizzaplanadmin.model.GioHang;
+import com.philipstudio.pizzaplanadmin.model.MonAn;
 import com.philipstudio.pizzaplanadmin.model.NguoiDung;
 
 import java.text.DecimalFormat;
@@ -25,6 +30,7 @@ public class ThongKeActivity extends AppCompatActivity {
 
     FirebaseDatabase firebaseDatabase;
     DatabaseReference dataRef;
+    RecyclerView rVThongKeTheoMonAn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,33 +43,35 @@ public class ThongKeActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 ArrayList<NguoiDung> nguoiDungArrayList = new ArrayList<>();
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     NguoiDung nguoiDung = dataSnapshot.getValue(NguoiDung.class);
                     nguoiDungArrayList.add(nguoiDung);
                 }
 
                 dataRef = firebaseDatabase.getReference().child("DonHang");
                 final ArrayList<DonHang> arrayList = new ArrayList<>();
-                for (int i=0; i<nguoiDungArrayList.size(); i++){
+                for (int i = 0; i < nguoiDungArrayList.size(); i++) {
                     dataRef.child(nguoiDungArrayList.get(i).getIdNguoiDung())
                             .addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            for (DataSnapshot dataSnapshot : snapshot.getChildren()){
-                                DonHang donHang = dataSnapshot.getValue(DonHang.class);
-                                arrayList.add(donHang);
-                            }
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                                        DonHang donHang = dataSnapshot.getValue(DonHang.class);
+                                        arrayList.add(donHang);
+                                    }
 
-                            txtTongSoDonHang.setText(String.valueOf(arrayList.size()));
-                            hienThiTongTienCacHoaDon(arrayList);
-                        }
+                                    txtTongSoDonHang.setText(String.valueOf(arrayList.size()));
+                                    hienThiTongTienCacHoaDon(arrayList);
+                                }
 
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
 
-                        }
-                    });
+                                }
+                            });
                 }
+
+                thongKeSoLuongBanDuocCacMonAn(nguoiDungArrayList);
             }
 
             @Override
@@ -73,9 +81,34 @@ public class ThongKeActivity extends AppCompatActivity {
         });
     }
 
-    private void hienThiTongTienCacHoaDon(ArrayList<DonHang> donHangs){
+    private void thongKeSoLuongBanDuocCacMonAn(ArrayList<NguoiDung> nguoiDungs) {
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        dataRef = firebaseDatabase.getReference().child("ChiTietDonHang");
+        for (int i = 0; i < nguoiDungs.size(); i++) {
+            dataRef.child(nguoiDungs.get(i).getIdNguoiDung())
+                    .addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            ArrayList<GioHang> gioHangs = new ArrayList<>();
+                            for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                                gioHangs = dataSnapshot.getValue(ChiTietDonHang.class).getListDanhSachGioHang();
+                            }
+
+                            Log.d("phuc", gioHangs.size() + " ");
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+        }
+    }
+
+
+    private void hienThiTongTienCacHoaDon(ArrayList<DonHang> donHangs) {
         double tongtien = 0;
-        for (int i=0; i<donHangs.size(); i++){
+        for (int i = 0; i < donHangs.size(); i++) {
             tongtien = tongtien + donHangs.get(i).getTongtien();
         }
 
@@ -84,9 +117,10 @@ public class ThongKeActivity extends AppCompatActivity {
         txtSoTienThuNhap.setText(formattedGiatien + " đồng");
     }
 
-    private void initView(){
+    private void initView() {
         txtTongSoDonHang = findViewById(R.id.textview_tongso_donhang);
         txtSoTienThuNhap = findViewById(R.id.textview_tongtien_thunhap);
+        rVThongKeTheoMonAn = findViewById(R.id.recyclerview_danhsach_thongke_monan);
         firebaseDatabase = FirebaseDatabase.getInstance();
     }
 }
